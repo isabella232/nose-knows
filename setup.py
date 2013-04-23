047aa107e7bfb9872f56db7cf837b0cd8be2dc94
Eyal Reuveni <eyal@eventbrite.com>
2013-04-23 14:06:02 -0700

    Bumping version to 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
-VERSION = '0.1'
+VERSION = '0.1.1'
 
 
 setup(
